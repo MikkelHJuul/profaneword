3f241a301cad685110572d03adbc65c1e5f521cc
Mikkel Juul <mju@dmi.dk>
2021-12-08 19:48:20 +0100

    more profanities
    and more on profanities

--- profanities/doc.go
+++ profanities/doc.go
@@ -2,7 +2,9 @@
 //swear words name-calling things etc. for use elsewhere.
 //
 //I have attempted to stay clear of racial slur and otherwise "offensive" stuff
-//(anti-homosexual etc.)
+//(anti-homosexual etc.) I have left in some fatshaming - it's all a balance
+// The sentences created should rather be funny than low-effort profane
+// thus "colorless flan" is alot funnier (and a lot cleverer) than "stupid fuck"
 //
 // some words are merely added for fun (as they would combine with others)
 //
