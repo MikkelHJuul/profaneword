fa77165d7c72b61a604133f03193909c94bbc92b
Mikkel Juul <mju@dmi.dk>
2021-12-20 09:28:35 +0100

    binds and docs
    for the command / CLI

--- profaneword/cmd/cmd.go
+++ profaneword/cmd/cmd.go
@@ -2,13 +2,17 @@ package cmd
 
 import (
 	"bufio"
+	"github.com/MikkelHJuul/profaneword"
+	"github.com/MikkelHJuul/profaneword/profanities"
 	"github.com/spf13/cobra"
 	"io"
 	"os"
+	"strings"
 )
 
 // usageTpl is a copy-paste of the underlying UsageTemplate in order to inject Args (and [..args])
-const usageTpl = `Usage:{{if .Runnable}}
+const (
+	usageTpl = `Usage:{{if .Runnable}}
   {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}} [..args]
   {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
 
@@ -30,35 +34,48 @@ Global Flags:
 Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
   {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
 
-Args:
-	1337  output formatted as 1337-speak
-	/s    sARcaSTiC OUtpUt
+Args[formatters]:
+	1337  		output formatted as 1337-speak
+	uber1337	output formatted with an extended 1337 alphabet
+	fat			output some t3xt wifth fat fringers
+	fst			otput sme tet writen wit haste
+	SCREAM		OUTPUT IS UPPERCASE
+	whisper		output is lowercased
+	/s    		sARcaSTiC OUtpUt
+	
+	randomly	the next formatter is applied only randomly (per word basis) threshold is 50:50
+	random		the next formatter is applied only randomly (per character basis) threshold is 50:50
+				both "random" and "randomly" are chainable onto themselves, 
+				though "randomly" must be before "random"
 
 
 Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
 `
-
+	alternateDelimiters = ".-/_:$%^+=!@'`,|<>\"~\\?*&"
+	RAND                = "RAND"
+)
 
 var (
 	profaneCmd = &cobra.Command{
-		Use: "profaneword",
+		Use:   "profaneword",
 		Short: "A generator for profane passwords as requested by u/gatestone",
-		Long:
-`profaneword is a CLI library and tool for generating obscene/profane passwords. 
+		Long: `profaneword is a CLI library and tool for generating obscene/profane passwords. 
 It's probably not particularly safe to use, as these passwords will be easy to brute force; 
 if an attacker knows you use this generator. But hey, it's just for fun.`,
-		Args: cobra.OnlyValidArgs,
+		Args:      cobra.OnlyValidArgs,
 		ValidArgs: formatters,
-		Run: profaneWords,
+		Run:       profaneWords,
+		PreRun:    validateArgs,
 	}
 
 	obscure = &cobra.Command{
-		Use: "obscure",
-		Short: "apply formatters on std in",
-		Long: "obscure applies formatters on std in thus you can format any text, or format an output given by profaneword",
-		Args: cobra.OnlyValidArgs,
+		Use:       "obscure",
+		Short:     "apply formatters on std in",
+		Long:      "obscure applies formatters on std in thus you can format any text, or format an output given by profaneword",
+		Args:      cobra.OnlyValidArgs,
 		ValidArgs: formatters,
-		Run: obscureFunc,
+		Run:       obscureFunc,
+		PreRun:    validateArgs,
 	}
 
 	version = &cobra.Command{
@@ -70,13 +87,68 @@ if an attacker knows you use this generator. But hey, it's just for fun.`,
 	}
 )
 
+func errUseEnd(cmd *cobra.Command, t string) {
+	cmd.PrintErrln(t, "\n")
+	_ = cmd.Usage()
+	os.Exit(1)
+}
+
+func validateArgs(cmd *cobra.Command, args []string) {
+	for i, arg := range args {
+		if formatter(arg) == random {
+			if i == len(args)-1 {
+				errUseEnd(cmd, `"random" cannot be used without a formatter`)
+			}
+			if formatter(args[i+1]) == randomly {
+				errUseEnd(cmd, `"random" cannot appear before "randomly"`)
+			}
+		}
+		if formatter(arg) == randomly {
+			if i == len(args)-1 {
+				errUseEnd(cmd, `"randomly" cannot be used without a formatter`)
+			}
+		}
+	}
+}
+
 func profaneWords(cmd *cobra.Command, args []string) {
 	numWords := numWordsFrom(cmd)
-	text := GetWords(numWords)
-	formatter := formatterOf(args)
+	delim := getDelimiter(cmd)
+	var disallowW = disallowedWords(cmd)
+	sentencer := profanities.NewProfanitySentencer(disallowW)
+	sentence := sentencer.GetSentence(numWords)
+	text := sentencer.Sentece(sentence)
+	formatter := formatterOf(args, profaneword.RandomTitleFormatter(), profaneword.DelimiterFormatterWith(delim))
 	cmd.Println(formatter.Format(text))
 }
 
+func disallowedWords(cmd *cobra.Command) (disallowed profanities.Word) {
+	no, _ := cmd.PersistentFlags().GetString("no")
+	for _, nope := range strings.Split(no, "|") {
+		if nope == "" {
+			continue
+		}
+		if nope == "MISSPELL" {
+			disallowed = disallowed | profanities.MISSPELL
+			continue
+		}
+		if nope == "POSITIVE" {
+			disallowed = disallowed | profanities.POSITIVE
+			continue
+		}
+		errUseEnd(cmd, "unknown dissallowed word: "+nope)
+	}
+	return
+}
+
+func getDelimiter(cmd *cobra.Command) (delim string) {
+	delim, _ = cmd.PersistentFlags().GetString("delimiter")
+	if delim == RAND {
+		idx := profaneword.CryptoRand{}.RandMax(len(alternateDelimiters))
+		delim = string(alternateDelimiters[idx])
+	}
+	return
+}
 
 func numWordsFrom(cmd *cobra.Command) int { //errs?
 	pflags := cmd.PersistentFlags()
@@ -92,20 +164,24 @@ func numWordsFrom(cmd *cobra.Command) int { //errs?
 
 func obscureFunc(cmd *cobra.Command, args []string) {
 	reader := bufio.NewReader(os.Stdin)
-	formatter := formatterOf(args)
+	delim := getDelimiter(cmd)
+	formatter := formatterOf(args, profaneword.RandomTitleFormatter(), profaneword.DelimiterFormatterWith(delim))
 	for {
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
+				if text != "" && text != "\n" {
+					cmd.Println(formatter.Format(text))
+				}
 				break
 			}
 			cmd.Println(err)
 			os.Exit(1)
 		}
-		cmd.Print(formatter.Format(text))
+		text = text[:len(text)-1] // remove the newline to not print two newlines
+		cmd.Println(formatter.Format(text))
 	}
 }
-
 
 // Execute executes the root command.
 func Execute() error {
@@ -116,10 +192,13 @@ func init() {
 	profaneCmd.AddCommand(version)
 	profaneCmd.AddCommand(obscure)
 
-	profaneCmd.PersistentFlags().Int16P("extensiveness","e",2, "how long (number of words) the password should be")
-	profaneCmd.PersistentFlags().Bool("extend",false, "lengthen the output (extensiveness+1)")
-	profaneCmd.PersistentFlags().Bool("EXTEND",false, "lengthen the output further (extensiveness+3)")
+	profaneCmd.PersistentFlags().Int16P("extensiveness", "e", 2, "how long (number of words) the password should be. Default is 2")
+	profaneCmd.PersistentFlags().Bool("extend", false, "lengthen the output (extensiveness+1)")
+	profaneCmd.PersistentFlags().Bool("EXTEND", false, "lengthen the output further (extensiveness+3)")
+
+	profaneCmd.PersistentFlags().StringP("delimiter", "d", " ", "a specific delimiter to use, or '"+RAND+"' for a randomly chosen one from: '"+alternateDelimiters+"'")
+
+	profaneCmd.PersistentFlags().String("no", "", "exclude types of words: can be MISSPELL, POSITIVE or a '|' separated text of those")
 
 	profaneCmd.SetUsageTemplate(usageTpl)
 }
-
