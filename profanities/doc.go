ca8c66ad95182bbdaa7fd2126da19eca33146417
Mikkel Juul <mju@dmi.dk>
2021-12-20 09:25:36 +0100

    some documentation

--- profanities/doc.go
+++ profanities/doc.go
@@ -1,14 +1,16 @@
 //profanities is a package that collects a couple of lists of
 //swear words name-calling things etc. for use elsewhere.
+//I have focused on creating memorable words and include most words of negative connotations;
+//While they may not be negative specifically, they are negatively laden.
 //
 //I have attempted to stay clear of racial slur and otherwise "offensive" stuff
-//(anti-homosexual etc.) I have left in some fatshaming - it's all a balance
+//(anti-homosexual etc.) I have left in some fat shaming - it's all a balance
 // The sentences created should rather be funny than low-effort profane
-// thus "colorless flan" is alot funnier (and a lot cleverer) than "stupid fuck"
+// thus "colorless flan" is alot funnier (and a lot cleverer) than "stupid fuck"; both are accessible
 //
-// some words are merely added for fun (as they would combine with others)
+// some words are merely added for fun (as they would combine with others to produce funny outputs: "aggressive kitten")
 //
-//the lists are compiled from several places:
+//the lists are compiled from several places, and I brainstorm; at this point finding new words is a pastime:
 //  https://www.noswearing.com/
 //  https://github.com/zacanger/profane-words/blob/master/words.json
 //
