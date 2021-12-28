3ed36e760b7e51fc626ad632287791ca77ed38cc
Mikkel Juul <mju@dmi.dk>
2021-12-28 22:39:16 +0100

    rework sentence
    programmatical implementation

--- profanities/sentences.go
+++ profanities/sentences.go
@@ -6,12 +6,16 @@ import (
 	"strings"
 )
 
+type sentnc struct {
+	format string
+	word   Word
+}
+
 //Sentence is a linked-list of formattable structures, each with a format string,
 //a word (type) that would fit there, and a pointer to the next part of the Sentence
 type Sentence struct {
-	format string
-	word   Word
-	next   *Sentence
+	sentnc
+	next *Sentence
 }
 
 func (s *Sentence) getPart(word string) string {
@@ -71,184 +75,69 @@ type SentenceFetcher interface {
 //GetSentence builds a sentence of arbitrary length by using the internal
 //flatSentence, recursively calling the internal map of flatSentence, and compiling a Sentence from it
 func (pw *ProfanitySentencer) GetSentence(numWords int) *Sentence {
-	sents, ok := sentences[numWords]
-	if !ok {
-		sentence := pw.GetSentence(5)
-		cpy := sentence
-		for cpy.next != nil {
-			cpy = cpy.next
-		}
-		nextSent := pw.GetSentence(numWords - 5)
-		nextSent.format = ` ` + nextSent.format
-		cpy.next = nextSent
-		return sentence
-	}
-	sentence := sents[pw.RandMax(len(sents))]
-	return buildSentence(sentence)
-}
 
-//will not panic! test: TestSentencesCorrectLength validates the length!
-func buildSentence(flat flatSentence) *Sentence {
-	var sentence *Sentence = nil
-	subst := `%s`
-	indexPS := strings.LastIndex(flat.string, subst)
-	var currFlat = flat
-	for {
-		format, next := currFlat.string[indexPS:], currFlat.string[:indexPS]
-		sentence = &Sentence{
-			format: format,
-			word:   currFlat.words[len(currFlat.words)-1],
-			next:   sentence,
-		}
-
-		indexPS = strings.LastIndex(next, subst)
-		if indexPS == -1 {
-			sentence.format = next + sentence.format
-			break
-		}
-		currFlat = flatSentence{
-			string: next,
-			words:  currFlat.words[:len(currFlat.words)-1],
-		}
+	randSent := func() (*Sentence, sent) {
+		idx := pw.RandMax(len(sentences))
+		s := sentences[idx]
+		return &Sentence{sentnc: s.sentnc}, s
 	}
-	return sentence
+	var cur *Sentence
+	var s sent
+	pos := not_last
+	for pos&not_last != 0 {
+		cur, s = randSent()
+		pos = s.sentPos
+	}
+	if strings.HasSuffix(cur.format, " ") {
+		cur.format = cur.format[:len(cur.format)-1]
+	}
+	numWords--
+	for ; numWords > 0; numWords-- {
+		prev := cur
+		cur, s = randSent()
+		cur.next = prev
+	}
+	return cur
 }
 
 const efe = EXCL | FILLER | END
+const all Word = 255
 
-type flatSentence struct {
-	string
-	words []Word
+type sentPos uint8
+
+const (
+	not_last sentPos = 16
+)
+
+type sent struct {
+	sentnc
+	sentPos
 }
 
-//TODO - programmatically?!
-var sentences = map[int][]flatSentence{
-	1: {
-		{`%s`, []Word{efe}},
-		{`%s-fucker`, []Word{efe}},
-		{`sex-%s`, []Word{efe}},
-		{`%s!`, []Word{efe}},
-		{`%s?`, []Word{efe}},
-		{`%s?!`, []Word{efe}},
-		{`%s!?`, []Word{efe}},
-		{`%s!!`, []Word{efe}},
-		{`%s...NOT!`, []Word{efe}},
-	},
-	2: {
-		{`%s %s`, []Word{efe | START, efe}},
-		{`%s sex-%s`, []Word{efe | START, efe}},
-		{`%s %s-fucker`, []Word{efe | START, efe}},
-		{`%s-%s`, []Word{efe | START, efe}},
-		{`%s vs %s`, []Word{efe, efe}},
-		{`%s vs. %s`, []Word{efe, efe}},
-		{`%s, %s`, []Word{efe | START, efe}},
-		{`%s: %s`, []Word{efe | START, efe}},
-		{`%s; %s`, []Word{efe | START, efe}},
-		{`%s - %s`, []Word{efe | START, efe}},
-		{`%s -> %s`, []Word{EXCL | END, EXCL | END}},
-		{`%s = %s`, []Word{EXCL | END, EXCL | END}},
-		{`%s == %s`, []Word{EXCL | END, EXCL | END}},
-		{`%s is %s`, []Word{EXCL | END, EXCL | END}},
-		{`%s or %s`, []Word{EXCL | END, EXCL | END}},
-		{`%s! %s`, []Word{EXCL | END, FILLER | END}},
-		{`%s-fucker! %s`, []Word{EXCL | END, FILLER | END}},
-		{`%s?! %s`, []Word{EXCL | END, FILLER | END}},
-		{`%s? %s!`, []Word{DEFAULT, EXCL | END}},
-		{`%s? sex-%s!`, []Word{DEFAULT, EXCL | END}},
-		{`%s? %s-fucker!`, []Word{DEFAULT, EXCL | END}},
-		{`%s? %s`, []Word{DEFAULT, EXCL | END}},
-	},
-	3: {
-		{`%s %s %s`, []Word{efe | START, FILLER, efe}},
-		{`%s %s sex-%s`, []Word{efe | START, FILLER, efe}},
-		{`%s %s %s-fucker`, []Word{efe | START, FILLER, efe}},
-		{`%s %s %s!`, []Word{efe | START, FILLER, efe}},
-		{`%s-%s %s!`, []Word{DEFAULT, FILLER, efe}},
-		{`%s-%s sex-%s!`, []Word{DEFAULT, FILLER, efe}},
-		{`%s-%s %s-fucker!`, []Word{DEFAULT, FILLER, efe}},
-		{`%s vs %s %s`, []Word{efe, FILLER, efe}},
-		{`%s %s vs. %s`, []Word{DEFAULT, efe, efe}},
-		{`%s %s or %s`, []Word{DEFAULT, efe, efe}},
-		{`%s, %s %s`, []Word{efe | START, DEFAULT, END}},
-		{`%s %s, %s`, []Word{efe | START, FILLER, END}},
-		{`%s %s, %s!`, []Word{efe | START, FILLER, END}},
-		{`%s %s, sex-%s!`, []Word{efe | START, FILLER, END}},
-		{`%s %s, %s-fucker!`, []Word{efe | START, FILLER, END}},
-		{`%s: %s %s`, []Word{efe | START, DEFAULT, efe}},
-		{`%s %s: %s`, []Word{efe | START, END, efe}},
-		{`%s; %s %s`, []Word{efe | START, DEFAULT, efe}},
-		{`%s - %s %s`, []Word{efe | START, DEFAULT, efe}},
-		{`%s %s - %s`, []Word{efe | START, END, efe}},
-		{`%s -> %s %s`, []Word{EXCL | END, FILLER, EXCL | END}},
-		{`%s %s! %s`, []Word{DEFAULT, EXCL | END, FILLER | END}},
-		{`%s? %s %s!`, []Word{START | FILLER, FILLER, EXCL | END}},
-		{`%s? %s %s`, []Word{START | FILLER, FILLER, EXCL | END}},
-		{`%s %s? %s!`, []Word{START | FILLER, FILLER, EXCL | END}},
-		{`%s %s? %s`, []Word{START | FILLER, FILLER, EXCL | END}},
-	},
-	4: {
-		{`%s %s %s %s`, []Word{efe | START, FILLER, FILLER, efe}},
-		{`%s %s %s sex-%s`, []Word{efe | START, FILLER, FILLER, efe}},
-		{`%s %s %s %s-fucker`, []Word{efe | START, FILLER, FILLER, efe}},
-		{`%s %s %s %s!`, []Word{efe | START, FILLER, FILLER, efe}},
-		{`%s vs %s %s %s`, []Word{efe, DEFAULT, FILLER, efe}},
-		{`%s vs. %s %s %s`, []Word{efe, DEFAULT, FILLER, efe}},
-		{`%s %s %s vs %s`, []Word{DEFAULT, FILLER, efe, efe}},
-		{`%s %s %s vs %s-fucker`, []Word{DEFAULT, FILLER, efe, efe}},
-		{`%s %s %s vs. %s`, []Word{DEFAULT, FILLER, efe, efe}},
-		{`%s %s %s vs. sex-%s`, []Word{DEFAULT, FILLER, efe, efe}},
-		{`%s %s vs %s %s`, []Word{DEFAULT, efe, DEFAULT, efe}},
-		{`%s %s vs. %s %s`, []Word{DEFAULT, efe, DEFAULT, efe}},
-		{`%s, %s %s %s`, []Word{efe | START, DEFAULT, FILLER, END}},
-		{`%s %s %s, %s`, []Word{efe | START, FILLER, FILLER, END}},
-		{`%s %s %s, %s!`, []Word{efe | START, FILLER, FILLER, END}},
-		{`%s %s %s, sex-%s!`, []Word{efe | START, FILLER, FILLER, END}},
-		{`%s %s %s, %s-fucker!`, []Word{efe | START, FILLER, FILLER, END}},
-		{`%s: %s %s %s`, []Word{efe | START, DEFAULT, FILLER, efe}},
-		{`%s %s: %s %s`, []Word{DEFAULT, efe, DEFAULT, efe}},
-		{`%s %s %s: %s`, []Word{efe | START, FILLER, END, efe}},
-		{`%s; %s %s %s`, []Word{efe | START, DEFAULT, FILLER, efe}},
-		{`%s? %s %s %s`, []Word{EXCL | END, DEFAULT, FILLER, efe}},
-		{`%s - %s %s %s`, []Word{efe | START, DEFAULT, FILLER, efe}},
-		{`%s %s! %s %s`, []Word{DEFAULT, efe, DEFAULT, efe}},
-		{`%s? %s %s %s!`, []Word{DEFAULT, DEFAULT, FILLER, EXCL | END}},
-		{`%s %s? %s %s!`, []Word{START | FILLER, FILLER, DEFAULT, EXCL | END}},
-		{`%s %s-fucker? %s %s!`, []Word{START | FILLER, FILLER, DEFAULT, EXCL | END}},
-		{`%s %s? %s %s`, []Word{START | FILLER, FILLER, DEFAULT, EXCL | END}},
-	},
-	5: {
-		{`%s %s %s %s %s`, []Word{efe | START, FILLER, FILLER, FILLER, efe}},
-		{`%s %s %s %s sex-%s`, []Word{efe | START, FILLER, FILLER, FILLER, efe}},
-		{`%s %s %s %s %s-fucker`, []Word{efe | START, FILLER, FILLER, FILLER, efe}},
-		{`%s %s %s %s %s!`, []Word{efe | START, FILLER, FILLER, FILLER, efe}},
-		{`%s vs %s %s %s %s`, []Word{efe, DEFAULT, FILLER, FILLER, efe}},
-		{`%s vs. %s %s %s %s`, []Word{efe, DEFAULT, FILLER, FILLER, efe}},
-		{`%s %s %s %s vs %s`, []Word{DEFAULT, FILLER, FILLER, efe, efe}},
-		{`%s %s %s %s vs sex-%s`, []Word{DEFAULT, FILLER, FILLER, efe, efe}},
-		{`%s %s %s %s vs %s-fucker`, []Word{DEFAULT, FILLER, FILLER, efe, efe}},
-		{`%s %s %s %s vs. %s`, []Word{DEFAULT, FILLER, FILLER, efe, efe}},
-		{`%s %s %s vs %s %s`, []Word{DEFAULT, FILLER, efe, DEFAULT, efe}},
-		{`%s %s %s vs. %s %s`, []Word{DEFAULT, FILLER, efe, DEFAULT, efe}},
-		{`%s, %s %s %s %s`, []Word{efe | START, DEFAULT, FILLER, FILLER, END}},
-		{`%s %s %s, %s %s`, []Word{efe | START, FILLER, FILLER, FILLER, END}},
-		{`%s %s %s, %s %s!`, []Word{efe | START, FILLER, FILLER, FILLER, END}},
-		{`%s %s %s, %s %s-fucker!`, []Word{efe | START, FILLER, FILLER, FILLER, END}},
-		{`%s %s %s, %s sex-%s!`, []Word{efe | START, FILLER, FILLER, FILLER, END}},
-		{`%s: %s %s %s %s`, []Word{efe | START, DEFAULT, FILLER, FILLER, efe}},
-		{`%s %s: %s %s %s`, []Word{DEFAULT, efe, DEFAULT, FILLER, efe}},
-		{`%s %s %s %s: %s`, []Word{efe | START, FILLER, FILLER, END, efe}},
-		{`%s %s %s %s: %s-fucker`, []Word{efe | START, FILLER, FILLER, END, efe}},
-		{`%s; %s %s %s %s`, []Word{efe | START, DEFAULT, FILLER, FILLER, efe}},
-		{`%s? %s %s %s %s`, []Word{EXCL | END, DEFAULT, FILLER, FILLER, efe}},
-		{`%s %s? %s %s %s`, []Word{EXCL | END, DEFAULT, FILLER, FILLER, efe}},
-		{`%s-%s? %s %s %s`, []Word{EXCL | END, DEFAULT, FILLER, FILLER, efe}},
-		{`%s - %s %s %s %s`, []Word{efe | START, DEFAULT, FILLER, FILLER, efe}},
-		{`%s %s - %s %s %s`, []Word{efe | START, DEFAULT, FILLER, FILLER, efe}},
-		{`%s %s %s! %s %s`, []Word{DEFAULT, FILLER, efe, DEFAULT, efe}},
-		{`%s %s? %s %s %s!`, []Word{DEFAULT, END, DEFAULT, FILLER, EXCL | END}},
-		{`%s %s %s? %s %s!`, []Word{START | FILLER, FILLER, END, DEFAULT, EXCL | END}},
-		{`%s %s %s? %s sex-%s!`, []Word{START | FILLER, FILLER, END, DEFAULT, EXCL | END}},
-		{`%s %s %s? %s %s-fucker!`, []Word{START | FILLER, FILLER, END, DEFAULT, EXCL | END}},
-		{`%s %s %s? %s %s`, []Word{START | FILLER, FILLER, END, DEFAULT, EXCL | END}},
-	},
+var sentences = [...]sent{
+	{sentnc: sentnc{format: `%s `, word: all}},
+	{sentnc: sentnc{format: `%s-fucker `, word: efe}},
+	{sentnc: sentnc{format: `%s-fucker! `, word: efe}},
+	{sentnc: sentnc{format: `sex-%s `, word: efe}},
+	{sentnc: sentnc{format: `sex-%s! `, word: efe}},
+	{sentnc: sentnc{format: `%s! `, word: all}},
+	{sentnc: sentnc{format: `%s? `, word: all}},
+	{sentnc: sentnc{format: `%s?! `, word: all}},
+	{sentnc: sentnc{format: `%s!? `, word: all}},
+	{sentnc: sentnc{format: `%s!! `, word: efe}},
+	{sentnc: sentnc{format: `%s...NOT! `, word: efe}},
+	{sentnc: sentnc{format: `%s-`, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s vs `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s vs. `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s, `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s: `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s; `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s - `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s -> `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s = `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s == `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s is `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s or `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s of `, word: all}, sentPos: not_last},
+	{sentnc: sentnc{format: `%s `, word: efe}},
 }
